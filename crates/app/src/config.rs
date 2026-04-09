1f0f67fad0fe0326d4454409310b8f39d2732e7c
Floranaras <carlosranara0@gmail.com>
2026-04-09 13:53:48 +0800

    fix: updated api link

--- crates/app/src/config.rs
+++ crates/app/src/config.rs
@@ -1,3 +1,4 @@
 pub const WORKER_URL: &str =
-    "https://ghfetch.carlosranara.workers.dev";
+    "https://ghfetch.carlosranara.workers.dev/v1/stats";
+
 pub const GITHUB_USERNAME: &str = "AmaneKai";
